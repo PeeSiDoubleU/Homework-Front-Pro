5fdbeddd79426dad6dc920097f60dd5cd054392d
PeeSiDoubleU <145497537+PeeSiDoubleU@users.noreply.github.com>
2024-02-25 23:36:56 +0100

    Homework-1 28.02.2024

---
+++ index.js
@@ -0,0 +1,35 @@
+// TASK #1
+
+
+// let userInput = prompt("Введите слово");
+
+// function alphabet_order (str) {
+//    let strSorted = str.split('').sort().join('');
+//    return strSorted
+// }
+
+// console.log(userInput);
+// console.log(alphabet_order(userInput));
+
+
+
+// TASK #2
+
+// let word = 'Make it beautiful guys';
+// let longestWord = '';
+
+// function findlongestword(str) {
+//    let strLongest = str.split(' ')
+
+//    strLongest.forEach(function (item) {
+
+//       if (item.length > longestWord.length) {
+//          longestWord = item;
+//       }
+
+//    });
+
+//    return longestWord
+// }
+
+// console.log(findlongestword(word));
